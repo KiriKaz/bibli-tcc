4371c8b38e9598f1883133c9151b4ad873d051f0
KiriKaz <igkepsi@gmail.com>
2020-03-18 12:05:02 -0300

    Login CSS

--- etc/handler/session.php
+++ etc/handler/session.php
@@ -1,5 +1,14 @@
 <?php
+if(session_status() == PHP_SESSION_NONE) session_start();
 
-session_start();
+$headers = getallheaders();
+$referrer = $headers['Referer'] ?? "..";
+
+$direto = basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"]);
+
+if($direto) {
+    header("Location: $referrer", true, 302);
+	die();
+}
 
 ?>
